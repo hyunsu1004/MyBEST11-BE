767f9eef0d1bd8cf85b907dce05e5c3e11f71a04
hyunsu1004 <chlgustn3680@naver,com>
2026-07-16 13:24:16 +0900

    add https:// to cors config

--- src/main/java/com/best11/config/SecurityConfig.java
+++ src/main/java/com/best11/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5173",
-                "my-best-11-fe-inky.vercel.app"));
+                "https://my-best-11-fe-inky.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
