bf29484e28baec9d4b4177487fafe9ab41c0704a
hyunsu1004 <chlgustn3680@naver,com>
2026-07-16 13:21:19 +0900

    Add vercel domain to CORS allowed origins

--- src/main/java/com/best11/config/SecurityConfig.java
+++ src/main/java/com/best11/config/SecurityConfig.java
@@ -57,7 +57,8 @@ public class SecurityConfig {
     @Bean   // 새로 추가
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173",
+                "my-best-11-fe-inky.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
